e26f85d31171fce3f82f2ea18a6db2865e11b7ed
jaspindersingh1 <jaspinders31@gmail.com>
2019-03-13 21:27:23 -0400

    add expo react app

--- PartyVite/src/components/Home/home.js
+++ PartyVite/src/components/Home/home.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { View, Text,} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    }
+});
 
 class Home extends React.Component {
     render() {
+        return(
         <View>
-            <Text>
+            <Text style={styles.welcome}>
                 Welcome to PartyVite!
             </Text>
         </View>
+        )
     }
 }
 
