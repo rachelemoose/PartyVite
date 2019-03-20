50ed7a9b6ebc0d576512aef6ab402ca1d9d47427
singmokjai <lijason001@gmail.com>
2019-03-19 23:28:50 -0400

    expo googlesignin

--- PartyVite/screens/LinksScreen.js
+++ PartyVite/screens/LinksScreen.js
@@ -9,6 +9,19 @@ export default class LinksScreen extends React.Component {
     title: '',
   };
 
+      if (result.type === "success") {
+        this.setState({
+          signedIn: true,
+          name: result.user.name,
+          photoUrl: result.user.photoUrl
+        })
+      } else {
+        console.log("cancelled")
+      }
+    } catch (e) {
+      console.log("error", e)
+    }
+  }
   render() {
     return (
       <ScrollView style={styles.container}>
@@ -22,10 +35,39 @@ export default class LinksScreen extends React.Component {
   }
 }
 
+const LoginPage = props => {
+  return (
+    <View>
+      <Text style={styles.header}>Sign In With Google</Text>
+      <Button title="Sign in with Google" onPress={() => props.signIn()} />
+    </View>
+  )
+}
+
+const LoggedInPage = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Welcome:{props.name}</Text>
+      <Image style={styles.image} source={{ uri: props.photoUrl }} />
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#A33856',
   },
-});
+  header: {
+    fontSize: 25
+  },
+  image: {
+    marginTop: 15,
+    width: 150,
+    height: 150,
+    borderColor: "rgba(0,0,0,0.2)",
+    borderWidth: 3,
+    borderRadius: 150
+  }
+})
