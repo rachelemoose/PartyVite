e2dc4ca5e57c3eb9aa20696bd2e5403c46b9d73b
R.E. Moose <rachelemoose@gmail.com>
2019-03-20 20:32:09 -0400

    send message file updated

--- PartyVite/send_sms.js
+++ PartyVite/send_sms.js
@@ -5,11 +5,33 @@ require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
+const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+const partyHostNumber = process.env.HOST_PHONE_NUMBER;
+const messagingService = process.env.TWILIO_MESSAGING_SERVICE_SID;
+// this array needs to popoulate with hosts's contacts 
+const invitees = ['+15716436981', '+17187535298']
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+15716654064',
-     to: '+15716436981'
-   })
-  .then(message => console.log(message.sid));
+let body = "Rachel Moose has sent you a PartyVite! Send 'Y' for Yes"
+
+// send one message to one number
+// client.messages
+//   .create({
+//      body: body,
+//      from: twilioNumber,
+//      to: invitees
+//    })
+//   .then(message => console.log(message.sid)).catch(err => console.error(err));
+
+Promise.all(
+  invitees.map(invitee => {
+    return client.messages.create({
+      to: invitee,
+      from: messagingService,
+      body: body
+    });
+  })
+)
+  .then(messages => {
+    console.log('Messages sent!');
+  })
+  .catch(err => console.error(err));
