892fd8440d7ab08146919ce95fb185befcf2a483
andriyana <andriyana2212@gmail.com>
2019-03-26 08:12:06 -0400

    updating the contacts to ask for permissions, adding the onclick file

--- PartyVite/getcontacts.js
+++ PartyVite/getcontacts.js
@@ -1,67 +1,60 @@
-import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import * as React from 'react';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
 import { Constants, Permissions, Contacts } from 'expo';
 
-export default class App extends Component {
-  
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.showFirstContactAsync}>
-          <Text style={styles.paragraph}>
-            Get first contact by ID!
-          </Text>
-        </TouchableOpacity>
-      </View>
-    );
+
+
+export default class App extends React.Component {
+  state ={
+    allContacts: [],
   }
-  
-  showFirstContactAsync = async () => {
-    // Ask for permission to query contacts.
+
+  componentDidMount () {
+    this.getContactsAsync();
+  }
+
+  async getContactsAsync() {
     const permission = await Permissions.askAsync(Permissions.CONTACTS);
-    if (permission.status !== 'granted') {
-      console.log("Permission denied!")
+    if (permission.status !== 'granted'){
       return;
     }
     const contacts = await Contacts.getContactsAsync({
-      fields: [
-        Contacts.PHONE_NUMBERS,
-        Contacts.EMAILS,
-      ],
-      pageSize: 10,
-      pageOffset: 0,
-    });
-    
-    if (contacts.total > 0) {
-      var firstId = contacts.data[0].id;
-    }
-    
-    const contactById = await Contacts.getContactByIdAsync({
-      fields: [
-        Contacts.PHONE_NUMBERS,
-        Contacts.EMAILS,
-      ],
-      id: firstId,
-    });
-    console.log("Fetched contact by id:" + JSON.stringify(contactById))
+      fields: [Contacts.PHONE_NUMBERS],
+      sort: Contacts.SortTypes.FirstName, 
+      pageSize: 1000, /* how many contacts to grab at one time */
+      pageOffset: 0, /* how many pages of contacts to skip */
+    })
+    this.setState({ allContacts: contacts.data });
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <ScrollView style={{flex: 1}}>
+        {this.state.allContacts.map((person, index) => (
+          <Text key={index} style ={styles.paragraph}>
+            {person.name}
+            {'\n'}
+            📞 {person.phoneNumbers? person.phoneNumbers[0].number: "no phone"}
+          </Text>
+        ))}
+      </ScrollView>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
+    padding: 8,
   },
   paragraph: {
-    margin: 24,
-    fontSize: 18,
-    padding: 15,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-    backgroundColor: '#ccccff',
+    margin: 10,
+    fontSize: 15,
+    textAlign: 'left',
   },
 });
