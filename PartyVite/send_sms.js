71b550d7aa853ffb73600457f8fd46bdefefe474
Rachel Moose <rachelemoose@gmail.com>
2019-03-25 21:16:52 -0400

    update send_sms.js

--- PartyVite/send_sms.js
+++ PartyVite/send_sms.js
@@ -9,7 +9,7 @@ const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 const partyHostNumber = process.env.HOST_PHONE_NUMBER;
 const messagingService = process.env.TWILIO_MESSAGING_SERVICE_SID;
 // this array needs to popoulate with hosts's contacts 
-const invitees = ['+15716436981', '+17187535298']
+
 
 let body = "Rachel Moose has sent you a PartyVite! Send 'Yes' if you can make it, 'No' if you can't attend, and 'Maybe', if you're not sure."
 
