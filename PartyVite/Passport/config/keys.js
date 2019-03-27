d278c3b09b52048fba0adc2a546c95f4bfd95ebd
singmokjai <lijason001@gmail.com>
2019-03-27 19:16:59 -0400

    deleted passport folder

--- PartyVite/Passport/config/keys.js
+++
@@ -1,9 +0,0 @@
-// add this file to .gitignore
-
-module.exports = {
-
-    google: {
-        clientID: "395252509329-m47j7ggdhjt639q630t852nui99cumm0.apps.googleusercontent.com",
-        clientSecret: "9TUfz16ladufkubqBGufu4Rw"
-    }
-}
