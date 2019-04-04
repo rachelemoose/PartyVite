de6e8f5e2c47ad42e69c2908adc0855fe66e9dd8
jaspindersingh1 <jaspinders31@gmail.com>
2019-04-03 21:58:34 -0400

    removed unnecessary comments from Linkscreen file and inviation file. then comment out third screen settings tab out for demo day and comment out setting json that wasnt relevant to ios

--- PartyVite/navigation/MainTabNavigator.js
+++ PartyVite/navigation/MainTabNavigator.js
@@ -39,22 +39,22 @@ LinksStack.navigationOptions = {
   ),
 };
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
+// const SettingsStack = createStackNavigator({
+//   Settings: SettingsScreen,
+// });
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
-};
+// SettingsStack.navigationOptions = {
+//   tabBarLabel: 'Settings',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+//     />
+//   ),
+// };
 
 export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
-  SettingsStack,
+  // SettingsStack,
 });
